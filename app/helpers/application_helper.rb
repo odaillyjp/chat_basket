c5bf0a27d6622cfec4ca147927a11359dc09a7fa
Suguru Odai <ourqwers@gmail.com>
2016-04-24 01:50:06 +0900

    Add helper for modifier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,8 @@
 module ApplicationHelper
-  def hiding_bem_class(class_name, condition)
-    suffix = condition ? '--disabled' : ''
-    [class_name, suffix].join
+  def add_modifier_class(original_name, modifier, condition)
+    return original_name unless condition
+
+    original_name_with_modifier = [original_name, modifier].join('--')
+    [original_name, original_name_with_modifier].join(' ')
   end
 end
