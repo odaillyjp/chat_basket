9a642ebf764e3cd50a21a28ec2925301b7d18ddb
Suguru Odai <ourqwers@gmail.com>
2016-07-14 18:11:48 +0900

    Rename add_modifier_class to class_name_with_modifier

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,5 @@
 module ApplicationHelper
-  def add_modifier_class(original_name, modifier, condition)
-    return original_name unless condition
-
-    original_name_with_modifier = [original_name, modifier].join('--')
-    [original_name, original_name_with_modifier].join(' ')
+  def class_name_with_modifier(original_name, modifier, condition)
+    condition ? [original_name, modifier].join('--') : original_name
   end
 end
