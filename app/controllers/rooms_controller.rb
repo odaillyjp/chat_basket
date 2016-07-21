cc748c3776f6b4ed82474233c81e4ff6dac6dab8
Suguru Odai <ourqwers@gmail.com>
2016-07-22 08:51:56 +0900

    Redirect to first room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,13 @@ class RoomsController < ApplicationController
   before_action :set_room, only: %i(show)
 
   def index
+    # TODO: 部屋作成機能が実装されるまでは最初の部屋にリダイレクト
+    if true
+      redirect_to room_path(Room.first)
+      return
+    end
+    # TODO: これ以降は実行されない
+
     @rooms = Room.order(:id)
   end
 
